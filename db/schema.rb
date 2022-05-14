7b2fb06a50e96ae72c24ff784e44658d32c31e70
Carrimbaccus Zafroollah <zafroollah@gmail.com>
2022-05-14 21:28:34 +0400

    Bookmark and List models generated.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_14_130615) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_14_172738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
